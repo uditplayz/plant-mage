cf2886424ae8a01889da3c7742bdc4baf7e2069a
Udit Jain <jain.udit0000@gmail.com>
2026-02-19 00:54:32 +0530

    fixed github action

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/plant-mage',
+  assetPrefix: '/plant-mage/',
   images: {
     unoptimized: true,
   },
